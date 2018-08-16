d49924cb95ed43ce9e8ae141d3c4e28161ad149d
vagrant <vagrant@homestead>
2018-08-16 23:36:15 +0000

    first commit

---
+++ database/seeds/ArtistsTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use App\Http\Model\Artist;
+
+class ArtistsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	for ($i = 1; $i <= 10; $i++) {
+    		Artist::create([
+	            'name' => str_random(10),
+		        'url' => 'https://google.com',
+		        'content' => 'アーティストに関する説明' . $i,
+	            'image' => 'images/tama.jpg'
+        	]);
+        }
+    }
+}
