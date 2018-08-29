013c9cfbdffab5731b00e583885a4c31b592e776
vagrant <vagrant@playground>
2018-08-29 09:41:14 +0000

    ルーティング設定

--- routes/web.php
+++ routes/web.php
@@ -6,22 +6,28 @@ Route::namespace('Web')->group(function () {
     Route::get('/', 'ArtistController@index')->name('artist.index');
     Route::get('/artist/{artist}', 'ArtistController@show')->name('artist.show');
 
-// アーティスト登録
-    Route::post('/artist/{artist}/register', 'ArtistController@register')->name('artist.register');
-    Route::post('/artist/{artist}/register/delete', 'ArtistController@registerDelete')->name('artist.register.delete');
+    Route::group(['middleware' => 'auth:user'], function() {
+        // アーティスト登録
+        Route::post('/artist/{artist}/register', 'ArtistController@register')->name('artist.register');
+        Route::post('/artist/{artist}/register/delete', 'ArtistController@registerDelete')->name('artist.register.delete');
 
-//認証
-    Auth::routes();
+        //認証
+        Auth::routes();
 
-//ユーザ
-    Route::get('/profile', 'HomeController@index')->name('profile');
-
+        //ユーザ
+        Route::get('/profile', 'HomeController@index')->name('profile');
+    });
 });
 
 // 管理者側ルート
 Route::namespace('Admin')->name('admin::')->prefix('admin')->group(function () {
 
-    Route::get('/', 'HomeController@index')->name('index');
+    Route::get('login',     'LoginController@showLoginForm')->name('login');
+    Route::post('login',    'LoginController@login');
 
-    Route::get('/crowler', 'HomeController@crowlerIndex')->name('crowler.index');
+    Route::middleware('auth:admin')->group(function () {
+        Route::get('/', 'HomeController@index')->name('index');
+        Route::post('logout',   'LoginController@logout')->name('logout');
+        Route::get('/crowler', 'HomeController@crowlerIndex')->name('crowler.index');
+    });
 });
