e3cd8feeb1013bcfe6ccdb1c80faa5136de620fa
n.kenji <n.kenji@dayneeds.jp>
2018-09-11 12:37:43 +0900

    詳細ページレイアウト修正

--- resources/views/artist/show.blade.php
+++ resources/views/artist/show.blade.php
@@ -7,36 +7,46 @@
 
     <div class="row">
         <div class="col-12 text-center">
-            <img class="logo" src="{{ $artist->img_url }}" alt="logo" width="300px" height="300px">
-            <p>{{ $artist->name }}</p>
-            <p><a href="{{ $artist->url }}" target="_blank">アーティストページへ</a></p>
+            <div class="box box-solid">
+                <div class="box-body">
+                    <img class="logo" src="{{ $artist->img_url }}" alt="logo" width="300px" height="300px">
+                    <h2>{{ $artist->name }}</h2>
+                    <p><a href="{{ $artist->url }}" target="_blank"><i class="fa fa-clone"></i>アーティストページへ</a></p>
+                </div>
+            </div>
+            <div class="box-footer">
+                @if (Auth::user())
+                    <input id="artistId" type="hidden" name="register" value="{{ Auth::user()->id ?? ''}}">
+                    @if ($artist->artist_register == '[]')
+                        <button id="artistRegister" class="btn btn-primary">登録する</button>
+                    @else
+                        <button id="artistDelete" class="btn btn-danger">登録解除</button>
+                    @endif
+                @endif
+            </div>
         </div>
     </div>
     <div class="row">
         <div class="col-md-12">
-            <h2>ライブ一覧</h2>
-            <table class="row" border="1">
-                <tr>
-                    <th>日付</th>
-                    <th class="col-6">ライブ</th>
-                </tr>
-                @foreach($artist->lives as $live)
-                    <tr>
-                        <td>{{ $live->date }}</td>
-                        <td>{{ $live->title }}</td>
-                    </tr>
-                @endforeach
-            </table>
+            <div class="box box-solid">
+                <div class="box-body">
+                    <h2>ライブ一覧</h2>
+                    <table class="row" border="1">
+                        <tr>
+                            <th>日付</th>
+                            <th class="col-6">ライブ</th>
+                        </tr>
+                        @foreach($artist->lives as $live)
+                            <tr>
+                                <td>{{ $live->date }}</td>
+                                <td>{{ $live->title }}</td>
+                            </tr>
+                        @endforeach
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
-    @if (Auth::user())
-        <input id="artistId" type="hidden" name="register" value="{{ Auth::user()->id ?? ''}}">
-        @if ($artist->artist_register == '[]')
-            <button id="artistRegister" class="btn btn-primary">登録する</button>
-        @else
-            <button id="artistDelete" class="btn btn-danger">登録解除</button>
-        @endif
-    @endif
     <script>
         $(function(){
             $id = $('#artistId').val();
