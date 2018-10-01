d4d25a90b177ccd6c59d306bebf2e2fdd59bb016
n.kenji <n.kenji@dayneeds.jp>
2018-10-01 13:51:09 +0900

    shared artist form

--- resources/views/admin/artist/create.blade.php
+++ resources/views/admin/artist/create.blade.php
@@ -6,45 +6,9 @@
         <div class="box box-solid">
             <div class="box-body">
                 <div class="row">
+                    @include('admin.shared.artist_form')
                     <div class="col-sm-6">
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="control-label">name</label>
-                            <input required class="form-control" id="name" name="name" value="{{ old('name') }}">
-                            @if ($errors->has('name'))
-                                <span class="help-block">{{ $errors->first('name') }}</span>
-                            @endif
-                        </div>
-                        <div class="form-group{{ $errors->has('url') ? ' has-error' : '' }}">
-                            <label for="url" class="control-label">URL</label>
-                            <input required class="form-control" id="url" name="url" value="{{ old('url') }}">
-                            @if ($errors->has('url'))
-                                <span class="help-block">{{ $errors->first('url') }}</span>
-                            @endif
-                        </div>
-                        <div class="form-group{{ $errors->has('selector') ? ' has-error' : '' }}">
-                            <label for="selector" class="control-label">selector</label>
-                            <input class="form-control" id="selector" name="selector" value="{{ old('selector') }}">
-                            @if ($errors->has('selector'))
-                                <span class="help-block">{{ $errors->first('selector') }}</span>
-                            @endif
-                        </div>
-                        <div class="form-group{{ $errors->has('title_selector') ? ' has-error' : '' }}">
-                            <label for="title_selector" class="control-label">title_selector</label>
-                            <input class="form-control" id="title_selector" name="title_selector" value="{{ old('title_selector') }}">
-                            @if ($errors->has('title_selector'))
-                                <span class="help-block">{{ $errors->first('title_selector') }}</span>
-                            @endif
-                        </div>
-                        <div class="form-group{{ $errors->has('date_selector') ? ' has-error' : '' }}">
-                            <label for="date_selector" class="control-label">date_selector</label>
-                            <input class="form-control" id="date_selector" name="date_selector" value="{{ old('date_selector') }}">
-                            @if ($errors->has('date_selector'))
-                                <span class="help-block">{{ $errors->first('date_selector') }}</span>
-                            @endif
-                        </div>
                         ファイル：<input type="file" name='main'><br/>
-                    </div>
-                    <div class="col-sm-6">
                         <div class="form-group{{ $errors->has('tags') ? ' has-error' : '' }}">
                             @php
                                 $artist_tag_ids = isset($artist) ? $artist->tags()->pluck('tags.id')->toArray() : [];
--- resources/views/admin/artist/edit.blade.php
+++ resources/views/admin/artist/edit.blade.php
@@ -6,43 +6,7 @@
         <div class="box box-solid">
             <div class="box-body">
                 <div class="row">
-                    <div class="col-sm-6">
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="control-label">name</label>
-                            <input required class="form-control" id="name" name="name" value="{{ $artist->name ?? old('name') }}">
-                            @if ($errors->has('name'))
-                                <span class="help-block">{{ $errors->first('name') }}</span>
-                            @endif
-                        </div>
-                        <div class="form-group{{ $errors->has('url') ? ' has-error' : '' }}">
-                            <label for="url" class="control-label">URL</label>
-                            <input required class="form-control" id="url" name="url" value="{{ $artist->url ?? old('url') }}">
-                            @if ($errors->has('url'))
-                                <span class="help-block">{{ $errors->first('url') }}</span>
-                            @endif
-                        </div>
-                        <div class="form-group{{ $errors->has('selector') ? ' has-error' : '' }}">
-                            <label for="selector" class="control-label">selector</label>
-                            <input class="form-control" id="selector" name="selector" value="{{ $artist->selector ?? old('selector') }}">
-                            @if ($errors->has('selector'))
-                                <span class="help-block">{{ $errors->first('selector') }}</span>
-                            @endif
-                        </div>
-                        <div class="form-group{{ $errors->has('title_selector') ? ' has-error' : '' }}">
-                            <label for="title_selector" class="control-label">title_selector</label>
-                            <input class="form-control" id="title_selector" name="title_selector" value="{{ $artist->title_selector ?? old('title_selector') }}">
-                            @if ($errors->has('title_selector'))
-                                <span class="help-block">{{ $errors->first('title_selector') }}</span>
-                            @endif
-                        </div>
-                        <div class="form-group{{ $errors->has('date_selector') ? ' has-error' : '' }}">
-                            <label for="date_selector" class="control-label">date_selector</label>
-                            <input class="form-control" id="date_selector" name="date_selector" value="{{ $artist->date_selector ?? old('date_selector') }}">
-                            @if ($errors->has('date_selector'))
-                                <span class="help-block">{{ $errors->first('date_selector') }}</span>
-                            @endif
-                        </div>
-                    </div>
+                    @include('admin.shared.artist_form')
                     <div class="col-sm-6">
                         <img src="{{ asset('storage/images/' . $artist->name . '/main.jpg') ?? asset('storage/images/no.jpg') }}" width="250px" height="250px"><br />
                         <input type="file" name='main'><br/>
